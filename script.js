780d917477dc3b730c6a0ad7f0a50110205875ee
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-02-06 20:47:00 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -48,17 +48,3 @@ document.getElementById('badgeButton').addEventListener('click', function() {
     // Substitua o alert com suas próprias lógicas de pop-up ou modal
 });
 
-
-// Script para download do currículo
-document.getElementById('downloadButton').addEventListener('click', function() {
-    var link = document.createElement('a');
-    link.href = 'Profile.pdf';
-    link.download = 'Profile.pdf';
-    link.click();
-});
-
-// Script para pop-up da badge
-document.getElementById('badgeButton').addEventListener('click', function() {
-    alert('Informações do Projeto:\nNome: Decodificador\nVersão: 1.0\nAutor: Carlos Turina');
-    // Substitua o alert com suas próprias lógicas de pop-up ou modal
-});
